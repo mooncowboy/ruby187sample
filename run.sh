3104a5d67d59de4ead2c598cbd408e207323122d
Ricardo Fiel <rifiel@microsoft.com>
2019-06-19 11:53:10 +0100

    Working sample app

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 3000:3000 -v C:/Users/rifiel/repos/_github_mine/ruby187sample:/app ruby187
+docker run -it --rm -p 3000:3000 -v C:/Users/rifiel/repos/_github_mine/ruby187sample:/app --env-file ./.env_file ruby187
